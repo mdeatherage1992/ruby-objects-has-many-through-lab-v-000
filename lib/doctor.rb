e579b4cd559bf98674f8906eaeca0b3a9f257f9b
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-23 11:49:07 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,8 +13,8 @@ class Doctor
     end
 
   def new_appointment(date,patient)
-    patient = Patient.new(patient)
-    new_appointment = Appointment.new(patient,self,date)
+    new_patient = Patient.new(patient)
+    new_appointment = Appointment.new(new_patient,self,date)
     @appointments << new_appointment
   end
 
