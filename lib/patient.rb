9f0d34a83c0f588a27b8d6a08518259ef71c7231
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-23 11:59:34 +0000

    automatically backed up by learn

---
+++ lib/patient.rb
@@ -0,0 +1,15 @@
+class patient
+  attr_accessor :name, :appointments
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+    @@all << self
+  end
+
+  def new_appointment(doctor,date)
+    new_appointment = Appointment.new(self,doctor,date)
+    @appointments << new_appointment
+  end
+    
